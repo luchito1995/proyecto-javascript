36a271fcbe49511586a1c484d6e9e3cd1b2cd6f8
luchito1995 <lucho_lp_1995@hotmail.com>
2023-04-14 19:21:03 -0300

    Commit 20

--- js/buscar.js
+++ js/buscar.js
@@ -1,126 +1,126 @@
 //Listado de los productos que voy a vender, en un array con objetos
-const disfraces = [
-    {
-        id: 1,
-        nombre: "Disfraz berenjena",
-        categoria: "Alimentos",
-        precio: 500,
-        imagen: "https://i.ibb.co/6FXLL3G/a1.jpg",
-        cantidad: 1,
-    },
-    {
-        id: 2,
-        nombre: "Disfraz Kim Jong-un",
-        categoria: "Montado",
-        precio: 2000,
-        imagen: "https://i.ibb.co/M8w0B7m/a2.jpg",
-        cantidad: 1,
-    },
-    {
-        id: 3,
-        nombre: "Disfraz Donald Trump",
-        categoria: "Montado",
-        precio: 1400,
-        imagen: "https://i.ibb.co/2sTsLdh/a3.jpg",
-        cantidad: 1,
-    },
-    {
-        id: 4,
-        nombre: "Disfraz koala",
-        categoria: "Animales",
-        precio: 600,
-        imagen: "https://i.ibb.co/zxYh6xM/a4.jpg",
-        cantidad: 1,
-    },
-    {
-        id: 5,
-        nombre: "Disfraz montado a dino",
-        categoria: "Animales",
-        precio: 800,
-        imagen: "https://i.ibb.co/hDbmJhc/a5.jpg",
-        cantidad: 1,
-    },
-    {
-        id: 6,
-        nombre: "Disfraz jet pack",
-        categoria: "Montado",
-        precio: 1200,
-        imagen: "https://i.ibb.co/5rwXy3s/a6.jpg",
-        cantidad: 1,
-    },
-    {
-        id: 7,
-        nombre: "Disfraz león",
-        categoria: "Animal",
-        precio: 1350,
-        imagen: "https://i.ibb.co/fq8jXyF/a7.jpg",
-        cantidad: 1,
-    },
-    {
-        id: 8,
-        nombre: "Disfraz tonto y retonto",
-        categoria: "Personajes",
-        precio: 2300,
-        imagen: "https://i.ibb.co/JH9y306/a8.jpg",
-        cantidad: 1,
-    },
-    {
-        id: 9,
-        nombre: "Disfraz oktoberfest",
-        categoria: "Festividades",
-        precio: 1750,
-        imagen: "https://i.ibb.co/RpLdh0G/a9.jpg",
-        cantidad: 1,
-    },
-    {
-        id: 10,
-        nombre: "Disfraz águila",
-        categoria: "Animales",
-        precio: 1500,
-        imagen: "https://i.ibb.co/BrWPMPB/a10.jpg",
-        cantidad: 1,
-    },
-    {
-        id: 11,
-        nombre: "Disfraz palta",
-        categoria: "Comida",
-        precio: 900,
-        imagen: "https://i.ibb.co/pZSNH08/a11.jpg",
-        cantidad: 1,
-    },
-    {
-        id: 12,
-        nombre: "Disfraz dino esqueleto",
-        categoria: "Halloween",
-        precio: 2050,
-        imagen: "https://i.ibb.co/51ZSwJz/a12.jpg",
-        cantidad: 1,
-    },
-    {
-        id: 13,
-        nombre: "Disfraz hombre de nieve",
-        categoria: "Festividades",
-        precio: 1900,
-        imagen: "https://i.ibb.co/XV3cwMW/a13.jpg",
-        cantidad: 1,
-    },
-    {
-        id: 14,
-        nombre: "Disfraz los increíbles",
-        categoria: "Personajes",
-        precio: 700,
-        imagen: "https://i.ibb.co/wKPRW2y/a14.jpg",
-        cantidad: 1,
-    },
-    {
-        id: 15,
-        nombre: "Disfraz rapto de alien",
-        categoria: "Halloween",
-        precio: 1000,
-        imagen: "https://i.ibb.co/1rNQsQx/a15.jpg",
-        cantidad: 1,
-    },
-];
+// const disfraces = [
+//     {
+//         id: 1,
+//         nombre: "Disfraz berenjena",
+//         categoria: "Alimentos",
+//         precio: 500,
+//         imagen: "https://i.ibb.co/6FXLL3G/a1.jpg",
+//         cantidad: 1,
+//     },
+//     {
+//         id: 2,
+//         nombre: "Disfraz Kim Jong-un",
+//         categoria: "Montado",
+//         precio: 2000,
+//         imagen: "https://i.ibb.co/M8w0B7m/a2.jpg",
+//         cantidad: 1,
+//     },
+//     {
+//         id: 3,
+//         nombre: "Disfraz Donald Trump",
+//         categoria: "Montado",
+//         precio: 1400,
+//         imagen: "https://i.ibb.co/2sTsLdh/a3.jpg",
+//         cantidad: 1,
+//     },
+//     {
+//         id: 4,
+//         nombre: "Disfraz koala",
+//         categoria: "Animales",
+//         precio: 600,
+//         imagen: "https://i.ibb.co/zxYh6xM/a4.jpg",
+//         cantidad: 1,
+//     },
+//     {
+//         id: 5,
+//         nombre: "Disfraz montado a dino",
+//         categoria: "Animales",
+//         precio: 800,
+//         imagen: "https://i.ibb.co/hDbmJhc/a5.jpg",
+//         cantidad: 1,
+//     },
+//     {
+//         id: 6,
+//         nombre: "Disfraz jet pack",
+//         categoria: "Montado",
+//         precio: 1200,
+//         imagen: "https://i.ibb.co/5rwXy3s/a6.jpg",
+//         cantidad: 1,
+//     },
+//     {
+//         id: 7,
+//         nombre: "Disfraz león",
+//         categoria: "Animal",
+//         precio: 1350,
+//         imagen: "https://i.ibb.co/fq8jXyF/a7.jpg",
+//         cantidad: 1,
+//     },
+//     {
+//         id: 8,
+//         nombre: "Disfraz tonto y retonto",
+//         categoria: "Personajes",
+//         precio: 2300,
+//         imagen: "https://i.ibb.co/JH9y306/a8.jpg",
+//         cantidad: 1,
+//     },
+//     {
+//         id: 9,
+//         nombre: "Disfraz oktoberfest",
+//         categoria: "Festividades",
+//         precio: 1750,
+//         imagen: "https://i.ibb.co/RpLdh0G/a9.jpg",
+//         cantidad: 1,
+//     },
+//     {
+//         id: 10,
+//         nombre: "Disfraz águila",
+//         categoria: "Animales",
+//         precio: 1500,
+//         imagen: "https://i.ibb.co/BrWPMPB/a10.jpg",
+//         cantidad: 1,
+//     },
+//     {
+//         id: 11,
+//         nombre: "Disfraz palta",
+//         categoria: "Comida",
+//         precio: 900,
+//         imagen: "https://i.ibb.co/pZSNH08/a11.jpg",
+//         cantidad: 1,
+//     },
+//     {
+//         id: 12,
+//         nombre: "Disfraz dino esqueleto",
+//         categoria: "Halloween",
+//         precio: 2050,
+//         imagen: "https://i.ibb.co/51ZSwJz/a12.jpg",
+//         cantidad: 1,
+//     },
+//     {
+//         id: 13,
+//         nombre: "Disfraz hombre de nieve",
+//         categoria: "Festividades",
+//         precio: 1900,
+//         imagen: "https://i.ibb.co/XV3cwMW/a13.jpg",
+//         cantidad: 1,
+//     },
+//     {
+//         id: 14,
+//         nombre: "Disfraz los increíbles",
+//         categoria: "Personajes",
+//         precio: 700,
+//         imagen: "https://i.ibb.co/wKPRW2y/a14.jpg",
+//         cantidad: 1,
+//     },
+//     {
+//         id: 15,
+//         nombre: "Disfraz rapto de alien",
+//         categoria: "Halloween",
+//         precio: 1000,
+//         imagen: "https://i.ibb.co/1rNQsQx/a15.jpg",
+//         cantidad: 1,
+//     },
+// ];
 
 
 
@@ -232,6 +232,11 @@ function mostrarResultados(resultados) {
         return;
     }
 
+    //Asegurarse de que resultados sea un array, sino esto me va a traer problemas en el .forEach
+    if (!Array.isArray(resultados)) {
+        resultados = [resultados]; //Convertir a un array
+    }
+
     resultados.forEach(disfraz => {
         const cardProducto = document.createElement("div");
         cardProducto.className = "card";
@@ -289,12 +294,26 @@ function mostrarResultados(resultados) {
     });
 }
 
-fetch("data.json")
+
+
+
+
+//Fetch
+const url = "../js/data.json";
+let disfraces = [];
+
+fetch(url)
 .then((resp) => resp.json())
 .then((data) => {
     console.log(data);
-    mostrarResultados(data); // Llamada a la función mostrarResultados con los datos obtenidos del fetch
+    mostrarResultados(data);
+})
+.catch((error) => {
+    console.error("Error al obtener datos JSON:", error);
 });
+
+
+
 
 
 //Mostrar los disfraces filtrados
